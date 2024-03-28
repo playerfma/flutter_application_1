5312c7881e3eccf1ee884423c813b5aac09713ce
playerfma <fabio_rp19@hotmail.com>
2024-03-28 20:02:35 -0300

    altercao no background

--- lib/main.dart
+++ lib/main.dart
@@ -10,11 +10,10 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+        home: Scaffold(
+            body: Center(
+              child: Text(' World!'),
+            ),
+            backgroundColor: Colors.blue));
   }
 }
